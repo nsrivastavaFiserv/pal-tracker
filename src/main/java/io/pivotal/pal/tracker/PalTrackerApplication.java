349ca147d552808673b7647a09eb14b3a7a4046b
Amit Singh <amit.singh17@emc.com>
2019-02-13 15:30:03 +0530

    changed datasource coding

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -18,9 +20,9 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    JdbcTimeEntryRepository timeEntryRepository() {
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    JdbcTimeEntryRepository timeEntryRepository(DataSource dataSource) {
+       /* MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));*/
         return new JdbcTimeEntryRepository(dataSource);
     }
 
